b6289b42ae9de956ecaa73453cf690a20ae98bef
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-08-29 13:46:28 +0500

    Populate Next Right Pointer Each Node I and II solutions

--- leetcode/PopulateNextRightPointerEachNodeII.py
+++ leetcode/PopulateNextRightPointerEachNodeII.py
@@ -10,27 +10,29 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        level_head = root
+        head = root
 
-        while level_head:
-            level_iteration = level_head
-            connector = None
-            next_node = None
-            while level_iteration:
-                if level_iteration.left:
-                    if connector is None:
-                        next_node = level_iteration.left
-                    else:
-                        connector.next = level_iteration.left
-                    connector = level_iteration.left
-                if level_iteration.right:
-                    if connector is None:
-                        next_node = level_iteration.right
-                    else:
-                        connector.next = level_iteration.right
-                    connector = level_iteration.right
-                level_iteration = level_iteration.next
-            level_head = next_node
+        while head:
+            # A dummy node to represent the start of the next level.
+            dummy = Node(0)
+            # `tail` will be used to build the `next` pointers for the next level.
+            tail = dummy
+
+            # `curr` iterates through the nodes of the current level.
+            curr = head
+            while curr:
+                # If a left child exists, append it to the next level's list.
+                if curr.left:
+                    tail.next = curr.left
+                    tail = tail.next
+                # If a right child exists, append it to the next level's list.
+                if curr.right:
+                    tail.next = curr.right
+                    tail = tail.next
+                # Move to the next node in the current level.
+                curr = curr.next
+            
+            # The next level starts at `dummy.next`.
+            head = dummy.next
+
         return root
-        
-        
