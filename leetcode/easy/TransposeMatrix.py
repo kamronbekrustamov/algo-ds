97d3990a9b4225472f0ec6ff07f258463228a9cf
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-09-14 01:34:29 +0500

    Transpose Matrix

---
+++ leetcode/easy/TransposeMatrix.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        result = [[0 for _ in range(m)] for _ in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                result[j][i] = matrix[i][j]
+        return result
+        
