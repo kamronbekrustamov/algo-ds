86346a7c858e3a9c4b9d55d1a14b37b755e5df3f
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-10-09 18:44:35 +0500

    Linked List Cycle

---
+++ leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,23 @@
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) {
+//         val = x;
+//         next = null;
+//     }
+// }
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow_pointer = head;
+        ListNode fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
