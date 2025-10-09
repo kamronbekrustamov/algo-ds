5032dbfb23fb9c76ccecd2c60be06c72b129189b
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-10-09 18:44:58 +0500

    Meeting Rooms

---
+++ leetcode/easy/MeetingRooms.java
@@ -0,0 +1,33 @@
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+        if (intervals == null || intervals.size() == 0) {
+            return true;
+        }
+        Collections.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval i1, Interval i2) {
+                return i1.start - i2.start;
+            }
+        });
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i).start < intervals.get(i - 1).end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
