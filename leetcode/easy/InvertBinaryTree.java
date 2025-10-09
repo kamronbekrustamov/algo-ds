b14c095347e254e31b49e43e0dbd93d9d5e5710d
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-10-09 10:27:45 +0500

    Invert Binary Tree

---
+++ leetcode/easy/InvertBinaryTree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Solution class for the Invert Binary Tree problem.
+ */
+class Solution {
+    /**
+     * Inverts a binary tree by swapping the left and right children of every node.
+     * This is solved using a recursive, depth-first approach.
+     *
+     * @param root The root of the binary tree to invert.
+     * @return The root of the inverted binary tree.
+     */
+    public TreeNode invertTree(TreeNode root) {
+        // Base case: If the node is null, we've reached the end of a branch.
+        if (root == null) {
+            return null;
+        }
+
+        // Recursively call invertTree on the original left and right children.
+        // It's important to complete the recursive calls before swapping the children
+        // of the current node.
+        TreeNode invertedRight = invertTree(root.right);
+        TreeNode invertedLeft = invertTree(root.left);
+
+        // Swap the left and right children of the current node.
+        root.left = invertedRight;
+        root.right = invertedLeft;
+
+        return root;
+    }
+}
