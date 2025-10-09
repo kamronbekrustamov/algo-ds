e6ee825fc8320a885943fc20cf514acbd220b0d1
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-10-09 18:47:47 +0500

    Reverse Linked List

---
+++ leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,45 @@
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode() {}
+//     ListNode(int val) { this.val = val; }
+//     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// }
+
+class Solution {
+    /**
+     * Reverses a singly linked list iteratively.
+     *
+     * This solution uses an iterative approach with two pointers:
+     * `prev_node`: the node that comes before the current one (starts as null).
+     * `current_node`: the node we are currently processing (starts as head).
+     *
+     * Algorithm:
+     * 1. Initialize `prev_node` to `null` and `current_node` to `head`.
+     * 2. Loop as long as `current_node` is not `null`:
+     *    a. Store the `next_node` of `current_node` before modifying `current_node.next`.
+     *    b. Reverse the `next` pointer of `current_node` to point to `prev_node`.
+     *    c. Move `prev_node` to `current_node`.
+     *    d. Move `current_node` to `next_node`.
+     * 3. After the loop, `current_node` will be `null`, and `prev_node` will be the new head of the reversed list.
+     *
+     * @param head The head of the singly linked list.
+     * @return The new head of the reversed linked list.
+     *
+     * Time Complexity: O(N), where N is the number of nodes in the linked list, as we traverse the list once.
+     * Space Complexity: O(1), as we only use a few extra pointers.
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev_node = null;
+        ListNode current_node = head;
+
+        while (current_node != null) {
+            ListNode next_node = current_node.next;
+            current_node.next = prev_node;
+            prev_node = current_node;
+            current_node = next_node;
+        }
+
+        return prev_node;
+    }
+}
